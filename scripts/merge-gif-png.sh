b5f3600d516672b3bb9fe83b0caed1aea890093c
Natty <31473117+AMNatty@users.noreply.github.com>
2022-11-20 18:51:21 +0100

    Renamed scripts to not have extensions

--- scripts/merge-gif-png.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-convert -layers composite $1 null: $2 -layers optimize -gravity center $3
--- scripts/tile-pngs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-ffmpeg -stream_loop 1 -i 00%02d.png -c:v png test.mov 
-ffmpeg -i test.mov -pix_fmt rgba -vf "format=rgba,select=not(mod(n\,1)),scale=128:128,tile=4x4" -frames:v 1 tile.png
